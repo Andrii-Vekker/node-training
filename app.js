a2e9d74a28265a9faaa116924906d6837205ce8d
Andrii-Vekker <vekker304@gmail.com>
2022-12-01 16:24:45 +0200

    awdsa

--- app.js
+++
@@ -1,3 +0,0 @@
-const foo = require("./index")
-
-foo("Boby")
--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-const foo = (data) => {
-    console.log(data)
+// const yargs = require("yargs");
+// const {hideBin} = require("yargs/helpers") with yargs
+//////////////////////////////////////////////////////////////////
+const { Command } = require("commander");
+const program = new Command();
+
+const contacts = require("./contacts")
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts)
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact)
+      break;
+
+    case "add":
+      const newBook = await contacts.addContact(name, email, phone);
+      console.log(newBook)
+      break;
+
+    case "remove":
+      const deleteContact = await contacts.removeCont(id);
+      console.log(deleteContact)
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 };
 
-module.expports = {
-foo
-}
+program
+ .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv)
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);       with yargs
+// invokeAction(argv);
+
+// invokeAction({action: "list"});
+// invokeAction({action: "get", id: "1"});
+// invokeAction({ action: "add", name: "1", email: "lox@emailcom", phone: "123124324" });
+// invokeAction({action: "remove", id: "9"});
